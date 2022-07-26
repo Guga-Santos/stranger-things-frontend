37039d72db6794606c98efacb65909c1e3a19ec8
GugaSantos <guga.santos.guga@gmail.com>
2022-07-26 15:21:59 -0300

    Feat: DEV mode

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      {process.env.DEV_MODE === 'true' && <h1>Em desenvolvimento</h1> }
       <StrangerThings />
     </div>
   );
