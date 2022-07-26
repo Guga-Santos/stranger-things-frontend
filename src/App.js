df1c7620998902b7dd10ceae93f82bed68007c60
GugaSantos <guga.santos.guga@gmail.com>
2022-07-26 15:32:08 -0300

    Dev mode false

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
-      {process.env.DEV_MODE === 'true' && <h1>Em desenvolvimento</h1> }
+      {process.env.REACT_APP_DEV_MODE === 'true' && <h1>Em desenvolvimento</h1> }
       <StrangerThings />
     </div>
   );
